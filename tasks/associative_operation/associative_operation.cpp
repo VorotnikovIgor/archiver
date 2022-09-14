c81699fc5facd8886a8040c956c8326af21014db
Igor Vorotnikov <iovorotnikov_1@edu.hse.ru>
2022-09-14 16:23:33 +0300

    Solved associative operation task

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -1,10 +1,11 @@
 #include "associative_operation.h"
+#include <vector>
 
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
-    for (size_t x = 0; x < table.size(); ++x){
-        for (size_t y = 0; y < table.size(); ++y){
-            for (size_t z = 0; z < table.size(); ++z){
-                if (table[table[x][y]][z] != table[x][table[y][z]]){
+    for (size_t x = 0; x < table.size(); ++x) {
+        for (size_t y = 0; y < table.size(); ++y) {
+            for (size_t z = 0; z < table.size(); ++z) {
+                if (table[table[x][y]][z] != table[x][table[y][z]]) {
                     return false;
                 }
             }
