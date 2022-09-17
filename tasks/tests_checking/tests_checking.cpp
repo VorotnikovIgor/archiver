ff83eea70d569fe98818ca1969690f737b7f1474
Igor Vorotnikov <iovorotnikov_1@edu.hse.ru>
2022-09-17 12:01:19 +0300

    solve tests_checking

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,6 +1,21 @@
 #include "tests_checking.h"
+#include <deque>
+#include <iostream>
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    return {};
+    std::deque<size_t> cur_order_in_idx;
+    for (size_t i = 0; i < student_actions.size(); ++i) {
+        if (student_actions[i].side == Side::Top) {
+            cur_order_in_idx.push_front(i);
+        }
+        else {
+            cur_order_in_idx.push_back(i);
+        }
+    }
+    std::vector<std::string> answer(queries.size());
+    for (size_t i = 0; i < queries.size(); ++i) {
+        answer[i] = student_actions[cur_order_in_idx[queries[i] - 1]].name;
+    }
+    return answer;
 }
