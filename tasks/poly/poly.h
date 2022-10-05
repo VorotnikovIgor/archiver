75d511b95720cfdde7f4f3c76da97cb16a38ed3f
Igor Vorotnikov <iovorotnikov_1@edu.hse.ru>
2022-10-05 15:29:15 +0300

    poly 1

--- tasks/poly/poly.h
+++ tasks/poly/poly.h
@@ -1,4 +1,23 @@
 #pragma once
+#include <vector>
+#include <cstdint>
+#include <iostream>
+#include <map>
 
 class Poly {
+    std::map<int64_t, int64_t> coefficients_;
+public:
+    Poly();
+    Poly(const std::vector<int64_t> &);
+    Poly(const std::vector<std::pair<int64_t, int64_t>> &);
+    int64_t operator()(const int64_t &) const;
+    Poly operator*(const Poly &) const;
+    Poly operator+(const Poly &) const;
+    Poly operator-(const Poly &) const;
+    void operator-=(const Poly &);
+    void operator+=(const Poly &);
+    bool operator==(const Poly &) const;
+    Poly operator-() const;
+    bool operator!=(const Poly &) const;
+    friend std::ostream &operator<<(std::ostream &os, const Poly&);
 };
