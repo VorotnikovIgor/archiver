30927269c5857d08dac34a7ea7647f7ee283beb6
Igor Vorotnikov <iovorotnikov_1@edu.hse.ru>
2022-10-06 18:02:10 +0300

    poly 10

--- tasks/poly/poly.h
+++ tasks/poly/poly.h
@@ -10,8 +10,8 @@ private:
 
 public:
     Poly();
-    explicit Poly(const std::initializer_list<int64_t> &);
-    explicit Poly(const std::initializer_list<std::pair<uint64_t, int64_t>> &);
+    Poly(const std::initializer_list<int64_t> &);
+    Poly(const std::initializer_list<std::pair<uint64_t, int64_t>> &);
     int64_t operator()(const int64_t &) const;
     Poly operator*(const Poly &) const;
     Poly operator+(const Poly &) const;
